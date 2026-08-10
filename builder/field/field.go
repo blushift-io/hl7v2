1a1a609662f2aa0425d5340b5f12852b01fa4d2b
Tom Tobias <ttobias@cheyrad.com>
2026-08-10 15:47:55 -0600

    feat(builder): implement field builder subpackage with ent-style builders

---
+++ builder/field/field.go
@@ -0,0 +1,95 @@
+package field
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/blushift-io/hl7v2"
+)
+
+type Builder struct {
+	rawhl7 hl7v2.RawField
+	err    error
+}
+
+func NewBuilder(rf hl7v2.RawField) *Builder {
+	return &Builder{rawhl7: rf}
+}
+
+func String(v string) *Builder {
+	return NewBuilder(hl7v2.RawField{
+		hl7v2.RawRepetition{
+			hl7v2.RawComponent{
+				hl7v2.RawSubcomponent(v),
+			},
+		},
+	})
+}
+
+func Int(v int) *Builder {
+	return String(fmt.Sprintf("%d", v))
+}
+
+func Bool(v bool) *Builder {
+	if v {
+		return String("Y")
+	}
+	return String("N")
+}
+
+type TimeBuilder struct {
+	*Builder
+	t      time.Time
+	layout string
+}
+
+func Time(t time.Time) *TimeBuilder {
+	tb := &TimeBuilder{
+		t:      t,
+		layout: "20060102150405",
+	}
+	tb.Builder = String(t.Format(tb.layout))
+	return tb
+}
+
+func (tb *TimeBuilder) Format(layout string) *TimeBuilder {
+	tb.layout = layout
+	tb.Builder = String(tb.t.Format(layout))
+	return tb
+}
+
+func Components(comps ...any) *Builder {
+	rawComps := make(hl7v2.RawRepetition, len(comps))
+	for i, c := range comps {
+		str := fmt.Sprintf("%v", c)
+		rawComps[i] = hl7v2.RawComponent{hl7v2.RawSubcomponent(str)}
+	}
+
+	return NewBuilder(hl7v2.RawField{rawComps})
+}
+
+func Repeated(reps ...any) *Builder {
+	rawReps := make(hl7v2.RawField, len(reps))
+	for i, r := range reps {
+		switch v := r.(type) {
+		case *Builder:
+			if len(v.rawhl7) > 0 {
+				rawReps[i] = v.rawhl7[0]
+			}
+		default:
+			str := fmt.Sprintf("%v", r)
+			rawReps[i] = hl7v2.RawRepetition{
+				hl7v2.RawComponent{hl7v2.RawSubcomponent(str)},
+			}
+		}
+	}
+
+	return NewBuilder(rawReps)
+}
+
+func (b *Builder) Build() hl7v2.RawField {
+	if b == nil {
+		return hl7v2.RawField{}
+	}
+	return b.rawhl7
+}
---
+++ builder/field/option.go
@@ -0,0 +1,48 @@
+package field
+
+import (
+	"fmt"
+
+	"github.com/blushift-io/hl7v2"
+)
+
+type ConditionalElse struct {
+	builder *Builder
+	cond    bool
+}
+
+func When(cond bool, val any) *ConditionalElse {
+	if cond {
+		switch v := val.(type) {
+		case *Builder:
+			return &ConditionalElse{builder: v, cond: true}
+		default:
+			return &ConditionalElse{builder: String(fmt.Sprintf("%v", val)), cond: true}
+		}
+	}
+	return &ConditionalElse{builder: NewBuilder(nil), cond: false}
+}
+
+func (ce *ConditionalElse) Else(val any) *Builder {
+	if ce.cond {
+		return ce.builder
+	}
+	switch v := val.(type) {
+	case *Builder:
+		return v
+	default:
+		return String(fmt.Sprintf("%v", val))
+	}
+}
+
+func (ce *ConditionalElse) Build() hl7v2.RawField {
+	return ce.builder.Build()
+}
+
+func Eval(fn func() any) *Builder {
+	res := fn()
+	if b, ok := res.(*Builder); ok {
+		return b
+	}
+	return String(fmt.Sprintf("%v", res))
+}
